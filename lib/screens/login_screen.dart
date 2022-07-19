d23ef17f4101cf4946cff44831c74a62e59c4456
abhizaan21 <abhi20560604@gmail.com>
2022-07-19 03:49:40 +0300

    first commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:kisanmol_app/screens/home_screen.dart';
+import 'package:kisanmol_app/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,51 +11,148 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   //form key
-  final _formKey= GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   //editing controller
-  final TextEditingController emailController= new TextEditingController();
-  final TextEditingController passwordController= new TextEditingController();
-
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final emailField= TextFormField(
+    final emailField = TextFormField(
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
-
-      onSaved: (value){
-        emailController.text=value!;
+      onSaved: (value) {
+        emailController.text = value!;
       },
       textInputAction: TextInputAction.next,
-
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.mail),
+        contentPadding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(21.0),
+        ),
+      ),
     );
-    final passwordField= TextFormField(
+
+    final passwordField = TextFormField(
       autofocus: false,
       controller: passwordController,
-
-      onSaved: (value){
-        passwordController.text=value!;
+      obscureText: true,
+      onSaved: (value) {
+        passwordController.text = value!;
       },
       textInputAction: TextInputAction.done,
-
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.lock_sharp),
+        contentPadding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(21.0),
+        ),
+      ),
     );
+
+    final loginButton = Material(
+      borderRadius: BorderRadius.circular(21),
+      color:Colors.lightGreen,
+      child: MaterialButton(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          minWidth: MediaQuery.of(context).size.width,
+          child: const Text('Login',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 16)),
+          /*onPressed: () async {
+              try {
+                final user =
+                await firebaseAuth.signInWithEmailAndPassword(
+                    email: email, password: password);
+                if (user != null) {
+                  Navigator.pushNamed(context, "intro_screen");
+                }
+              } catch (e) {
+                if (kDebugMode) {
+                  print(e);
+                }
+              }
+            },*/
+          onPressed: () {
+            print('pressed');
+          },
+        ),
+    );
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
         child: SingleChildScrollView(
           child: Container(
-            color: Colors.white,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children:<Widget> [
-                  emailField,
-                  passwordField,
-                ],
+            height:MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/background1.jpg'),
+              fit: BoxFit.cover,colorFilter: ColorFilter.mode(
+                  Colors.white,
+                  BlendMode.darken,
+                )
+            )),
+            child: Padding(
+              padding: const EdgeInsets.all(21.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 221,
+                        child: Image.asset('assets/images/Logo.png')),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    emailField,
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    passwordField,
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    loginButton,
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          "Don't have an account? ",
+                          style: TextStyle(fontSize: 14, color: Colors.black54),
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegistrationScreen()));
+                            },
+                            child: const Text(
+                              "SignUp!",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.green,
+                                  decoration: TextDecoration.underline),
+                            ))
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
